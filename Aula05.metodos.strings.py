b8d41ba163d219395d774c91e3fb0944a3ce3400
Shaaman331 <tarcisio2010souza@gmail.com>
2021-10-05 13:38:23 -0300

    Métodos de Strings, upper, lower, strip, lstrip, rstrip, find, replace.

---
+++ Aula05.metodos.strings.py
@@ -0,0 +1,10 @@
+"Métodos comuns de string"
+
+nome_curso = 'Edição de Video'
+print(nome_curso.upper()) #Tudo em maiúsculo
+print(nome_curso.lower()) #Tudo em minúsculo
+print(nome_curso.strip()) #Remover os espaços iniciasi e finais em uma palavra
+print(nome_curso.lstrip()) # Remover os espaços da esquerda
+print(nome_curso.rsplit()) #Remover os espaços da direita
+print(nome_curso.find('Video')) #Encontra a palavra pelo indice
+print(nome_curso.replace('Video', 'Música')) #Subistituir palavras 
