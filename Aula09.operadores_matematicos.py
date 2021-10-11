5a4d865a1770e53489be5ee9ffd6b3c719874c51
Shaaman331 <tarcisio2010souza@gmail.com>
2021-10-11 14:27:15 -0300

    Operadores matemáticos.

---
+++ Aula09.operadores_matematicos.py
@@ -0,0 +1,22 @@
+'''Operadores Matemáticos
+'''
+import math
+
+
+pizza = 30.50
+pedaco_pizza = 5
+print(pedaco_pizza + pedaco_pizza) #soma
+print(pedaco_pizza * 2) #multiplicação
+print(pedaco_pizza - pedaco_pizza) #subtração
+print(pizza /2) #divisão
+print(10 // 6) # divisão de inteiro
+print(10 % 6) #modulo 
+
+#Atalhos
+a = 10
+a = a + 5 
+a += 5
+
+#Arredondar valores
+print(round(5.7))
+print(math.ceil(2.2)) #arredonda para  cima
