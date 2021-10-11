fa15cbaa5ce12b8752be4ac3c81de99a1aaa2009
Shaaman331 <tarcisio2010souza@gmail.com>
2021-10-11 12:38:20 -0300

    Fatiamento de strings.

---
+++ Aula07.fatiando.string.py
@@ -0,0 +1,12 @@
+link = 'facebook.com/tarcisio'
+print(link[0])
+print(link[-1])
+print(link[0:5])
+print(link[1])
+print(link[-5:])
+print(link[5:])
+print(link[:-3])
+
+link1 = 'instagram/deveaprender'
+print(link1[10:])
+
