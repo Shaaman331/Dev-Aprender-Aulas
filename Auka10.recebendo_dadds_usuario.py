d23b58097f5b63eb5f713b7927f62abbe9622e16
Shaaman331 <tarcisio2010souza@gmail.com>
2021-10-11 14:44:32 -0300

    Função Int e Input.

---
+++ Auka10.recebendo_dadds_usuario.py
@@ -0,0 +1,32 @@
+''''
+Função Input 
+
+* O input()função permite a entrada do usuário. 
+
+* Use o parâmetro prompt para escrever uma mensagem antes da entrada: 
+
+Função Int
+
+* O int()função converte o especificado valor em um número inteiro
+
+* Converte uma string em valor inteiro
+'''
+print('Função Input')
+print('Enter your name:')
+x = input()
+print('Hello, ' + x) 
+print()
+
+x = input('Enter your name:')
+print('Hello, ' + x)
+
+print(input('Onde vocẽ mora ?'))
+print()
+
+print('Função Int')
+x = int(3.5)
+print(x)
+print()
+
+buscar_paginas = int(input('Quantas páginas buscar?'))
+print(type(buscar_paginas))
