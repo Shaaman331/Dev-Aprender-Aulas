edcafc32aee230d35f85870cd47358aa5d9ec363
Shaaman331 <tarcisio2010souza@gmail.com>
2021-11-01 09:51:26 -0300

    Modulos Python

---
+++ modulos/aulao1.modulo.py
@@ -0,0 +1,18 @@
+'''
+Use um Módulo
+* Agora podemos usar o módulo que acabamos de criar, usando o import tela: 
+
+
+
+Nota: Ao usar uma função de um módulo, use a sintaxe: module_name.function_name .
+'''
+import tela #Inportando modulo tela 
+from time import sleep #importando a biblioteca time acessando módulo sleep
+
+print('Modulos')
+
+tela.definir_claridade_tela() #Acessando a função tela.definir_claridade_tela
+sleep(5) # 
+
+tela.definir_desligamento_atomatico() #Acessando a função tela.definir_desligamento_automatico
+
---
+++ modulos/tela.py
@@ -0,0 +1,16 @@
+'''O que é um módulo?
+* Considere um módulo igual a uma biblioteca de código.
+Um arquivo que contém um conjunto de funções que você deseja incluir em seu aplicativo. 
+
+Crie um Módulo
+* Para criar um módulo basta salvar o código que deseja em um arquivo com a extensão de arquivo .py:
+
+Exemplo: tela.py 
+'''
+
+def definir_claridade_tela(): #Criando módulo definir_claridade_tela
+    print('Alterar claridade da tela')
+
+def definir_desligamento_automatico(): #Criando módulo definir_desligamento_automatico
+    print('Definir desligamento automático')
+    
